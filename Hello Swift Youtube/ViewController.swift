b8c73ef2a915d17fff789966407a09076eda70d9
Juan Trías <juan.trias@intelygenz.com>
2014-08-21 10:39:27 +0200

    Initial Commit

---
+++ Hello Swift Youtube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello Swift Youtube
+//
+//  Created by Juan on 21/08/14.
+//  Copyright (c) 2014 IGZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
